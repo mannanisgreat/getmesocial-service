153e2b1d1b8674f301f5bf6d08152f0b49002b48
mannanisgreat <mannanisgreat@gmail.com>
2021-01-24 22:33:05 +0530

    first commit

---
+++ src/main/java/com/example/Getmesocialservice/GetmesocialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Getmesocialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmesocialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmesocialServiceApplication.class, args);
+	}
+
+}
